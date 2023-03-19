7da9bd929535133e40812c1fa5fefae1e0416591
Himanshu Garg <himanshugarg5480@gmail.com>
2023-03-19 16:14:54 +0530

    third commit initialize body-parser in src/index and install mysql2, sequelize, sequelize-cli packages

---
+++ readme.md
@@ -0,0 +1,3 @@
+-> create a .env file in the main root and mention the port number 
+
+    port:"port_number"
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 const express = require("Express");
-const {port} = require("./config/serverConfig")
+const bodyParser = require("body-parser");
+const {port} = require("./config/serverConfig");
+
 const setupAndStartServer = async () => {
+
   const app = express();
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended:ture}));
+  
   app.listen(port, () => {
     console.log(`server started at ${port}`);
   });
